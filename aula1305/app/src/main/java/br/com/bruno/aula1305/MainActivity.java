041bdce2bea8cf4e1e8d82d36e79ad68aa890fed
Bruno Demartini <brunorisso86@gmail.com>
2019-05-13 21:01:40 -0300

    aula1305

---
+++ aula1305/app/src/main/java/br/com/bruno/aula1305/MainActivity.java
@@ -0,0 +1,20 @@
+package br.com.bruno.aula1305;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void calcular(View view) {
+        TextView tv = findViewById(R.id.valor);
+        tv.setText("8");
+    }
+}
