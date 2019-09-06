0911b24e5771e7a708eb737c00280cede06e14cc
Bruno Demartini <brunorisso86@gmail.com>
2019-09-05 21:47:52 -0300

    Parte grafica pronta

---
+++ listaTarefas/app/src/main/java/br/com/bruno/listatarefas/telas/RegisterActivity.java
@@ -0,0 +1,33 @@
+package br.com.bruno.listatarefas.telas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import br.com.bruno.listatarefas.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button registrar = findViewById(R.id.registrar_btn);
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Criando um intent explícito
+                // Recupera-se o contexto da view
+                // Chama-se explicitamente a classe da activity que deseja abrir
+                Intent intent = new Intent(v.getContext(), LoginActivity.class);
+
+                // Inicia-se a activity
+                startActivity(intent);
+            }
+        });
+
+    }
+}
